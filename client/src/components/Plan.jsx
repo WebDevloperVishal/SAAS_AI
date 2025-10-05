648c63b05d82f75703a21186b0718d7c13fa71dd
WebDevloperVisha <vishalsanam2024@gmail.com>
2025-10-05 12:54:52 +0530

    SAAS

--- client/src/components/Plan.jsx
+++ client/src/components/Plan.jsx
@@ -11,7 +11,7 @@ function Plan() {
             </div>
 
             <div className='mt-14 max-sm:mx-8'>
-                <PricingTable/>
+                {/* <PricingTable/> */}
             </div>
         </div>
     )
